93ed89f180e3db2238206763cf050e041f7d8775
Todd Stiers Sensys <tstiers@openvpn-client.ovpn.sonic.net>
2017-11-07 10:17:40 -0800

    Initial Commit

---
+++ xcode9icontestTests/xcode9icontestTests.swift
@@ -0,0 +1,36 @@
+//
+//  xcode9icontestTests.swift
+//  xcode9icontestTests
+//
+//  Created by Todd Stiers Sensys on 11/7/17.
+//  Copyright © 2017 Todd Stiers Sensys. All rights reserved.
+//
+
+import XCTest
+@testable import xcode9icontest
+
+class xcode9icontestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
